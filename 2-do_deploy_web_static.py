5bb57f2ea8d74fc4475092778340181074353873
MissTipo <dorine.a.tipo@gmail.com>
2023-01-01 08:14:55 +0300

    do deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,17 +3,6 @@
 A script that distributes an archive to your web servers,
 using the function do_deploy
 """
-
-"""if archive_path == NULL:
-    return False
-
-directories /tmp/, //upload to
-            /data/web_static/current /// uncompress archive
-symbolic link /data/web_static/current //delete
-              /data/web_static/current //create - linked to new version:
-            /data/web_static/releases/<archive filename without extension>
-execute remotely  env.hosts = ['<IP web-01>', 'IP web-02']"""
-
 from fabric.api import *
 from datetime import datetime
 env.hosts = ['54.90.55.176', '54.144.155.28']
@@ -24,6 +13,7 @@ def do_deploy(archive_path):
     distributes an archive to your web servers,
     using the function do_deploy if not archive_path:
     """
+    if not archive_path:
         return False
     # Upload the archive to the /tmp/ directory of the web server
     with cd('/tmp'):
@@ -35,10 +25,13 @@ def do_deploy(archive_path):
     file = file_name.split('.')[0]
     # Create a subdirectory
     run('mkdir -p /data/web_static/releases/{}'.format(file))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(file_name, file))
+    tf = /tmp/{}
+    run('tar -xzf tf -C /data/web_static/releases/{}'.format(file_name, file))
     # delete the uploaded .tgz archive from the /tmp/ directory on the
     run('rm /tmp/{}'.format(file_name))
-    run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}'.format(file, file))
+    des = /data/web_static/releases/{}
+    run('mv des/web_static/* ' 'des'.format(file, file))
     run('rm -rf /data/web_static/current')
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(file))
+    releases = /data/web_static/releases/{}/
+    current = /data/web_static/current
+    run("ln -s releases current".format(file))

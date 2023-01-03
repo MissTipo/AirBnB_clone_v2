05c85fb1a453b8d279423e34acd2c39db43db226
MissTipo <dorine.a.tipo@gmail.com>
2023-01-03 11:01:13 +0300

    fix error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,45 +1,39 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from
-the contents of the web_static folder"""
+
+"""
+A script that distributes an archive to your web servers,
+using the function do_deploy
+"""
 from fabric.api import *
 from datetime import datetime
-
-
-env.hosts = ['54.90.55.176', "54.144.155.28"]
+env.hosts = ['54.90.55.176', '54.144.155.28']
 
 
 def do_deploy(archive_path):
-    """generates a .tgz archive from the contents of the web_static"""
+    """
+    distributes an archive to your web servers,
+    using the function do_deploy if not archive_path:
+    """
     if not archive_path:
         return False
-    # Upload to /tmp/ in the server
-    # put(archive_path, "/tmp/")
-    with cd("/tmp/"):
-        res = put(archive_path, "/tmp/")
-        print(res)
 
-    # Make directory for the file extraction
-    run("mkdir -p /data/web_static/releases/")
+    # Upload the archive to the /tmp/ directory of the web server
+    with cd('/tmp'):
+        upload = put(archive_path, '/tmp/')
+        print(upload)
+    run('mkdir -p /data/web_static/releases/')
+    file_name = archive_path.split('/')[-1]
+    # return filename without the extension
+    file = file_name.split('.')[0]
+    # Create a subdirectory
+    run('mkdir -p /data/web_static/releases/{}'.format(file))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'
+        .format(file_name, file))
+    # delete the uploaded .tgz archive from the /tmp/ directory on the
+    run('rm /tmp/{}'.format(file_name))
 
-    # GEt the file name
-    file_name = archive_path.split("/")[-1]
-
-    # Extract the file from archive
-    remote_name = file_name.split(".")[0]
-    run("mkdir -p /data/web_static/releases/{}".format(remote_name))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
-        .format(file_name, remote_name))
-
-    # Delete the archive from the web serve
-    run("rm /tmp/{}".format(file_name))
-
-    # Move file out of web_static
-    run("mv /data/web_static/releases/{}/web_static/* "
-        "/data/web_static/releases/{}/".format(remote_name, remote_name))
-
-    # Delete the symbolic link
-    run("rm -rf /data/web_static/current")
-
-    # Create a new the symbolic link
+    run('mv -u /data/web_static/releases/{}/web_static/* '
+     '/data/web_static/releases/{}'.format(file, file))
+    run('rm -rf /data/web_static/current')
     run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(remote_name))
+        .format(file))

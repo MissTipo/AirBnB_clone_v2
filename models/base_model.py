5fd584f85c16bb4c1bd634606d33d528b8c68969
MissTipo <dorine.a.tipo@gmail.com>
2023-01-14 07:40:40 +0300

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -35,6 +35,8 @@ class BaseModel:
                     del kwargs['__class__']
             if 'id' not in kwargs.keys():
                 self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
             self.__dict__.update(kwargs)
             #for key, value in kwargs.items():
              #   setattr(self, key, value)

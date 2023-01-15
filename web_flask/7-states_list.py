ff402598fa121ccedd2ab013cdf39a4ae3349b7d
MissTipo <dorine.a.tipo@gmail.com>
2023-01-15 18:40:20 +0300

    fix errors

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def list_of_states():
     """Displays a HTML page with the list of all State objects"""
-    states = storage.all('State').values()
+    states = storage.all('State')
     # states = sorted(states, key=lambda x x.name)
     # states= sort(attribute='state.name')
     return render_template('7-states_list.html', states=states)

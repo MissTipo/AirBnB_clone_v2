b0ac84feb7c2e471b43f88a587bf9a0f35a4f1b7
MissTipo <dorine.a.tipo@gmail.com>
2023-01-15 15:59:21 +0300

    add empty line

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
+"""Starts a Flask web application that listens on 0.0.0.0, poert 5000 """
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbn():
-    """ Returns Hello HBNB! from 0.0.0.0:5000 """
+def hello_hbnb():
+    """Displays Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Returns HBNB from 0.0.0.0:5000/hbnb """
+    """Displays HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """ Returns C followed by the value of text """
+def c_is_fun(text):
+    """Displays C followed by the value of text"""
     text = text.replace('_', ' ')
     return "C {}".format(text)
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python', strict_slashes=False, defaults={'text': 'is cool'})
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """ Returns Python followed by the vale of the text """
+def python_is_cool(text="is cool"):
+    """Displays Python followed by the vale of the text"""
     text = text.replace('_', ' ')
     return "Python {}".format(text)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """ Returns n is a number if n is an integer """
-    return "{} is a number".format(n)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
+"""Starts a Flask web application listening on 0.0.0.0, port 5000"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbn():
-    """ Returns Hello HBNB! from 0.0.0.0:5000 """
+    """Displays Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Returns HBNB from 0.0.0.0:5000/hbnb """
+    """Displays HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """ Returns C followed by the value of text """
+def c_is_fun(text):
+    """Dispays C followed by the value of text """
     text = text.replace('_', ' ')
     return "C {}".format(text)
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python', strict_slashes=False, defaults={'text': 'is cool'})
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """ Returns Python followed by the vale of the text """
+def python_is_cool(text="is cool"):
+    """Displays Python followed by the vale of the text """
     text = text.replace('_', ' ')
     return "Python {}".format(text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """ Returns n is a number if n is an integer """
+def is_it_a_number(n):
+    """Displays n is a number if n is an integer """
     return "{} is a number".format(n)
 
 

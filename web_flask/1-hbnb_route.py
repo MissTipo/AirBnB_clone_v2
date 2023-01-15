6c1c298d83ebfc915b15017b0cab9e669a023288
MissTipo <dorine.a.tipo@gmail.com>
2023-01-15 14:58:32 +0300

    fix pycodestyle error

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
+"""Starts a Flask web application that listens on 0.0.0.0, port 5000"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbn():
-    """ Returns Hello HBNB! from 0.0.0.0:5000 """
+def hello_hbnb():
+    """Displays Hello HBNB1"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Returns HBNB from 0.0.0.0:5000/hbnb """
+    """Displays HBNB"""
     return "HBNB"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)

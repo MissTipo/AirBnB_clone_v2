ad022c9dd0a2022bea5e5a2a634bd152c4b6d147
MissTipo <dorine.a.tipo@gmail.com>
2023-01-15 17:28:12 +0300

    list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application that listens on 0.0.0.0, port 5000
+"""
+from flask import *
+from models.__init__ import storage
+
+app = Flask(__name__)
+
+
+#@app.route('/states_list/<value>', strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
+def list_of_states():
+    """Displays a HTML page with the list of all State objects"""
+    states = storage.all('State').values()
+    # states = sorted(states, key=lambda x x.name)
+    states= sort(attribute='state.name')
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def tear_down(exception=None):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()

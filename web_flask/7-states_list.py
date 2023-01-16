77b69a542788cc6c8b46565e16a8b289dd1c3d1a
MissTipo <dorine.a.tipo@gmail.com>
2023-01-16 05:43:10 +0300

    fix errors

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ Starts a Flask web application that listens on 0.0.0.0, port 5000
 from flask import *
 from models import storage
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -13,7 +14,7 @@ def tear_down(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def list_of_states():
     """Displays a HTML page with the list of all State objects"""
     states = storage.all('State')

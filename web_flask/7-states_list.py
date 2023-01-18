345a84fed272c1dfe4325974f17d16c22a564a13
MissTipo <dorine.a.tipo@gmail.com>
2023-01-18 10:42:43 +0300

    fix output errors

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,12 +4,13 @@ Starts a Flask web application that listens on 0.0.0.0, port 5000
 """
 from flask import *
 from models import storage
+from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def tear_down(exception):
+def tear_down(self):
     """Removes the current SQLAlchemy Session"""
     storage.close()
 
@@ -18,9 +19,13 @@ def tear_down(exception):
 def list_of_states():
     """Displays a HTML page with the list of all State objects"""
     states = storage.all('State')
-    # states= sort(attribute='state.name')
     return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def tear_down(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5000)
